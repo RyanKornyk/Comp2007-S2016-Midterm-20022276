89350bbdaf4c0b034f6cff8be27198dd47170901
Ryan <rkor23@gmail.com>
2016-06-23 18:32:24 -0400

    Recopied the database stuff which somehow broke

--- COMP2007-S2016-MidTerm/Models/TodoModel.Designer.cs
+++ COMP2007-S2016-MidTerm/Models/TodoModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Ryan\Downloads\COMP2007-S2016-MidTerm\COMP2007-S2016-MidTerm\Models\TodoModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Tom\Documents\COMP2007\COMP2007-S2016-MidTerm\COMP2007-S2016-MidTerm\Models\TodoModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- COMP2007-S2016-MidTerm/TodoDetails.aspx.cs
+++ COMP2007-S2016-MidTerm/TodoDetails.aspx.cs
@@ -5,13 +5,51 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+// using statements that are required to connect to DB
+using COMP2007_S2016_MidTerm.Models;
+using System.Web.ModelBinding;
+using System.Linq.Dynamic;
+
 namespace COMP2007_S2016_MidTerm
 {
     public partial class TodoDetails : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            // if loading the page for the first time get the data
+            if (!IsPostBack)
+            {
+                // Get the To Do data
+                this.GetTodo();
+            }
+        }
+        /**
+         * <summary>
+         * This method gets the ToDo data from the DB
+         * </summary>
+         * 
+         * @method GetToDo
+         * @returns {void}
+         */
+        protected void GetTodo()
+        {
+            // populate teh form with existing data from the database
+            int todoID = Convert.ToInt32(Request.QueryString["todoID"]);
+
+            // connect to the EF DB
+            using (TodoConnection db = new TodoConnection())
+            {
+                // populate a student object instance with the StudentID from the URL Parameter
+                Todo updatedToDo = (from student in db.Todos
+                                          where student.TodoID == todoID
+                                          select Todo).FirstOrDefault();
+
+                // map the student properties to the form controls
+                if (updatedToDo != null)
+                {
+
+                }
+            }
         }
     }
 }
